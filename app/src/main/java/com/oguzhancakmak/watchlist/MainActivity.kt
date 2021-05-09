c8ef653031ea9cb287a96e6a1011e826a44bb397
AhmetHamdiCengiz <66183595+AhmetHamdiCengiz@users.noreply.github.com>
2021-05-09 17:25:56 +0300

    Updated

--- app/src/main/java/com/oguzhancakmak/watchlist/MainActivity.kt
+++ app/src/main/java/com/oguzhancakmak/watchlist/MainActivity.kt
@@ -13,6 +13,10 @@ class MainActivity : AppCompatActivity() {
         Log.e("dasd","dljasdjdkl")
         Log.e("ahmdet","dljasdjdkl")
 
+        Log.e("ahmet","dljasdjdkl")
+        Log.e("dasd","dljasdjdkl")
+        Log.e("ahmdet","dljasdjdkl")
+
         Log.e("Tag","line12")
         Log.e("Tag","line13")
         Log.e("Tag","line14")
