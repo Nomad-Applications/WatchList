41e3f5130fb12ebb813534fda007644489755a94
Oğuzhan <oguzhan.ck98@gmail.com>
2021-05-09 17:20:25 +0300

    Test Commit

--- app/src/main/java/com/oguzhancakmak/watchlist/MainActivity.kt
+++ app/src/main/java/com/oguzhancakmak/watchlist/MainActivity.kt
@@ -2,10 +2,15 @@ package com.oguzhancakmak.watchlist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.e("Tag","line12")
+        Log.e("Tag","line13")
+        Log.e("Tag","line14")
     }
 }
